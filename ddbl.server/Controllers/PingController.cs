f6a00cdb0411687ab0c26788da4c581e880c14c8
DanielMigchels <danielm12@hotmail.nl>
2020-02-18 12:14:46 +0100

    Feature: Add Ping feature

---
+++ ddbl.server/Controllers/PingController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ddbl.server.Controllers
+{
+    [ApiController]
+    public class PingController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<PingController> _logger;
+
+        public PingController(ILogger<PingController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("/")]
+        public IActionResult Ping()
+        {
+            return Ok("Pong!");
+        }
+    }
+}
