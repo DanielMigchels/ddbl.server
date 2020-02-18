fb845d13e4f63e291eabeabe15c4ea283b54c772
DanielMigchels <danielm12@hotmail.nl>
2020-02-18 12:14:59 +0100

    Chore: Add CORS

--- ddbl.server/Startup.cs
+++ ddbl.server/Startup.cs
@@ -26,6 +26,13 @@ namespace ddbl.server
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddCors(o => o.AddPolicy("CORS", builder =>
+            {
+                builder.AllowAnyOrigin()
+                       .AllowAnyMethod()
+                       .AllowAnyHeader();
+            }));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +42,8 @@ namespace ddbl.server
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseCors("CORS");
 
             app.UseHttpsRedirection();
 
